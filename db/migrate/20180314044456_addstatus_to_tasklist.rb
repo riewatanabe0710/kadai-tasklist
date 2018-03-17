609b43bae1223a33ba9b22b1e7902b66dd5de590
Jiro.Kirameki <ec2-user@ip-172-31-3-255.us-east-2.compute.internal>
2018-03-17 15:21:57 +0000

    column

--- db/migrate/20180314044456_addstatus_to_tasklist.rb
+++
@@ -1,5 +0,0 @@
-class AddstatusToTasklist < ActiveRecord::Migration[5.0]
-  def change
-     add_column :tasks, :status, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314044456) do
+ActiveRecord::Schema.define(version: 20180317150705) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20180314044456) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
+    t.string   "status"
   end
 
 end
